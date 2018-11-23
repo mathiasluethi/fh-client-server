b75acf47a5f8d715108e5743d0de1d77664fa84b
Sebastian Richner <srichner@outlook.com>
2018-11-23 22:37:16 +0100

    add basic script

--- client/script.js
+++ client/script.js
@@ -12,3 +12,27 @@ function testClick() {
 socket.on('response', function (response) {
     console.log(response);
 });
+
+socket.on('game_state', function (gameState) {
+    console.log('game_state received', gameState);
+    updateQuestion();
+    updateUI();
+});
+
+socket.on('next_question', function (question) {
+
+});
+
+function startGame() {
+    console.log('game started');
+    socket.emit('start_game');
+}
+
+function updateQuestion() {
+    console.log('question updated');
+}
+
+function updateUI() {
+    console.log('ui updated');
+}
+
